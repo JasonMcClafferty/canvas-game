039a9ef86935dcf7d726a12ad035f78b7ef13962
jasonmcclafferty <L00130446@student.lyit.ie>
2019-02-21 17:28:24 +0000

    Linked and tested basic js - canvas integration
    Signed-off-by: jasonmcclafferty <L00130446@student.lyit.ie>

--- canvas.js
+++ canvas.js
@@ -1,3 +1,21 @@
-//window.onload = setup;
+window.onload = test;
 
-//var setup = function() {}
+// var setup = function() {}
+
+
+
+
+function clCanvas() {
+    ctx.clearRect(0, 0, 300, 300);
+}
+
+function test() {
+
+    var ctx = document.getElementById('canvas').getContext('2d');
+
+    ctx.save();
+
+    ctx.strokestyle = 'black';
+
+    ctx.fillRect(25, 25, 10, 10);
+}
