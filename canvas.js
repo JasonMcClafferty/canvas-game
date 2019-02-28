e831ae6e08795bf01a96833c8c26b461d09b41e5
jasonmcclafferty <L00130446@student.lyit.ie>
2019-02-28 14:50:57 +0000

    Added snake style edges, calibrated chaser speed

--- canvas.js
+++ canvas.js
@@ -10,22 +10,25 @@ var goalX,
 
 var chX,
     chY,
-    speed;
+    chaseSpeed;
 
 var ctx, stopGame;
 
 var keys = {};
 
-var lines = {
-
-};
+var dir;
 
 
 // Making sure the page is set up correctly before the game starts.
 window.onload = setup;
 
-// Using onkey instead of new event handling to allow
-// simultaneous movement across 2 dimensions.
+/*  Using onkey instead of new event handling to allow
+ *  simultaneous movement across 2 dimensions.
+ *
+ *  Changes keys boolean values to true/false,
+ *  the values are then accessed in the update
+ *  and player moves accordingly.
+ */
 window.onkeydown = function(e) {
 
     var kc = e.keyCode;
@@ -67,8 +70,7 @@ window.onkeyup = function(e) {
 }
 
 
-// couple the game loop to the browser
-
+// couple the game loop to the browser event loop
 window.main = function () {
     stopGame = window.requestAnimationFrame(main);
 
@@ -102,14 +104,17 @@ function setup() {
 
     goalSize = 50;
 
-
     chX =  450;
     chY = 80;
+    chaseSpeed = 2;
 
+    dir = directionVector(chX, chY, plX, plY);
+
+    // Spacebar closes the program,
+    // stops the browser event loop calling main().
     document.addEventListener('keydown', function(event) {
 
         if (event.keyCode == 32) {
-
             clCanvas();
             window.cancelAnimationFrame(stopGame);
         }
@@ -125,20 +130,44 @@ function setup() {
 function update() {
     //console.log('update');
 
+    /*  See lines 31, 51 for keys.direction use
+     *
+     *  If player goes out of bounds,
+     *  snake style teleportation to the other side of the canvas.
+     *  -   Adds an avoidance mechanic as the chaser
+     *      is faster than the player.
+     */
+    // s
     if (keys.up) {
         plY -= speed;
+
+        if (plY < -8) {
+            plY = 500;
+        }
     }
 
     if (keys.down) {
         plY += speed;
+
+        if (plY > 500) {
+            plY = 0;
+        }
     }
 
     if (keys.left) {
         plX -= speed;
+
+        if (plX < 0) {
+            plX = 500;
+        }
     }
 
     if (keys.right) {
         plX += speed;
+
+        if (plX > 500) {
+            plX = -10;
+        }
     }
 
 
@@ -151,14 +180,16 @@ function update() {
         resetGoal();
     }
 
-    if (plX > (chX + 1)
-        && plX < (chX + 13)
-        && plY > (chY +  1)
-        && plY < (chY + 13))
-    {
-        score -2;
+    if ((plX ) >= (chX)
+        && (plX) <= (chX)
+        && (plY) >= (chY)
+        && (plY)<= (chY)) {
+
+        score -= 2;
         resetChaser();
     }
+
+    chase();
 
 }
 
@@ -171,8 +202,9 @@ function render () {
 
     ctx.font = "20px Exo 2";
     ctx.fillText("Score: " + score, 15, 15);
-    var dir = directionVector(chX, chY, plX, plY).toString();
-    ctx. fillText(dir, 400, 15);
+
+    var dirStr = directionVector(chX, chY, plX, plY).toString();
+    ctx. fillText(dirStr, 400, 15);
 }
 
 
@@ -188,11 +220,11 @@ function draw() {
     ctx = document.getElementById('canvas').getContext('2d');
     ctx.save();
 
-
     drawPlayer();
     drawGoal()
     drawChaser();
 
+    //myDebug();
 
 }
 
@@ -209,7 +241,6 @@ function drawGoal() {
     // ctx.fillStyle = 'red';
     ctx.fillRect(goalX, goalY, goalSize, goalSize);
 }
-
 function resetGoal() {
     goalX = Math.floor(Math.random() * 450);
     goalY = Math.floor(Math.random() * 450);
@@ -227,7 +258,6 @@ function resetChaser() {
 
     // Begin follow algorithm
 }
-
 
 /*
     Finding the direction vector to give the chaser
@@ -251,3 +281,30 @@ function unitVector(x) {
 
     return [Math.sqrt(x[0]^2 + x[1]^2) * x[0], Math.sqrt(x[0]^2 + x[1]^2) * x[1]];
 }*/
+
+
+/*
+    Function to chase the player
+ */
+function chase() {
+    if (chX < plX) {
+        chX += chaseSpeed;
+    }
+
+    else if (chX > plX) {
+        chX -= chaseSpeed;
+    }
+
+    if (chY < plY) {
+        chY += chaseSpeed;
+    }
+
+    else if (chY > plY) {
+        chY -= chaseSpeed;
+    }
+}
+
+
+function myDebug() {
+
+}
